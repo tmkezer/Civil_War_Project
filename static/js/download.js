911663a127a44e63395a5a85958440f8cafb5a50
kingcrow15 <61525901+kingcrow15@users.noreply.github.com>
2020-07-08 20:40:54 -0700

    bug fixes and style correction

--- static/js/download.js
+++ static/js/download.js
@@ -1,55 +1,70 @@
-// function download(filename, text) {
-//     var element = document.createElement('a');
-//     element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-//     element.setAttribute('download', filename);
-
-//     element.style.display = 'none';
-//     document.body.appendChild(element);
-
-//     element.click();
-
-//     document.body.removeChild(element);
-// }
-
-// // Start file download.
-// document.getElementById("dwn-btn").addEventListener("click", function(){
-//     // Generate download of hello.txt file with some content
-//     var text = document.getElementById("text-val").value;
-//     var filename = "hello.txt";
-    
-//     download(filename, text);
-// }, false);
 $(document).ready(function () {
-    $('#load_data').click(function () {
-      $.ajax({
-        url: "/static/downloaddata/civil_war_data_hmdb.csv",
-        dataType: "text",
-        success: function (data) {
-          var hmdb_data = data.split(/\r?\n|\r/);
-          var table_data = '<table id="example"  class="table table-bordered table-striped">';
-          for (var count = 0; count < hmdb_data.length; count++) {
-            var cell_data = hmdb_data[count].split(",");
-            table_data += '<tr>';
-            for (var cell_count = 0; cell_count < cell_data.length; cell_count++) {
-              if (count === 0) {
-                table_data += '<th>' + cell_data[cell_count] + '</th>';
-              }
-              else {
-                if (cell_count === 6) {
-                  table_data += '<td>' + '<a href=' + cell_data[cell_count] + ' target="_blank">' + cell_data[cell_count] + '</a>' + '</td>';
-                }
-                else
-                  table_data += '<td>' + cell_data[cell_count] + '</td>';
-              }
-            }
-            table_data += '</tr>';
-          }
-          table_data += '</table>';
-          $('#hmdb_table').html(table_data);
-          // $('#example').DataTable();
-          // $('#example').DataTable();
+  $('#load_data').click(function () {
+    $.ajax({
+      url: "static/downloaddata/civil_war_data_hmdb.csv",
+      dataType: "text",
+      success: function (data) {
+        var printCounter = 0;
+        var hmdb_data = data.split(/\r?\n|\r/);
+        var table_data = '<table id="example"  class="table table-bordered table-striped">';
+        table_data += '<thead>';
+        table_data += '<tr>';
+        var count = 0;
+        var cell_data = hmdb_data[count].split(",");
+        for (var cell_count = 0; cell_count < cell_data.length; cell_count++){
+          table_data += '<th>' + cell_data[cell_count] + '</th>';
         }
-      });
-    }); 
-    console.log($().jquery); 
+        table_data += '</tr>';
+        table_data += '</thead>';
+        for (var count = 1; count < hmdb_data.length; count++) {
+          var cell_data = hmdb_data[count].split(",");
+          table_data += '<tr>';
+          for (var cell_count = 0; cell_count < cell_data.length; cell_count++) {
+            if (count === 0) {
+              table_data += '<th>' + cell_data[cell_count] + '</th>';
+            }
+            else {
+              if (cell_count === 6) {
+                table_data += '<td>' + '<a href=' + cell_data[cell_count] + ' target="_blank">' + cell_data[cell_count] + '</a>' + '</td>';
+              }
+              else
+                table_data += '<td>' + cell_data[cell_count] + '</td>';
+            }
+          }
+          table_data += '</tr>';
+        }
+        table_data += '</table>';
+        $('#hmdb_table').html(table_data);
+        // $('#example').DataTable();
+        $('#example').DataTable( {
+          dom: 'Bfrtip',
+          buttons: [
+              'copy',
+              {
+                  extend: 'excel',
+                  messageTop: 'The information in this table is copyright to Sirius Cybernetics Corp.'
+              },
+              {
+                  extend: 'pdf',
+                  messageBottom: null
+              },
+              {
+                  extend: 'print',
+                  messageTop: function () {
+                      printCounter++;
+                      if ( printCounter === 1 ) {
+                          return 'This is the first time you have printed this document.';
+                      }
+                      else {
+                          return 'You have printed this document '+printCounter+' times';
+                      }
+                  },
+                  messageBottom: null
+              }
+          ]
+      } )
+      }
+    });
   });
+  console.log($().jquery); 
+});
--- static/js/logic3.js
+++ static/js/logic3.js
@@ -1,11 +1,14 @@
 // Dataset we will be using to set the height of our rectangles.
 console.log("Logic3 Loaded");
+
+setTimeout(function(){  
+
 d3.json("/api/v1.0/bargraph", function(data) {
   
+  // console.log(data)
   // data
   var dataNumber = [];
   var dataTitle = [];
-
   for (var j = 0; j < data.length; j++) {
     dataNumber.push(data[j].number);
     dataTitle.push(data[j].title);
@@ -15,80 +18,104 @@ d3.json("/api/v1.0/bargraph", function(data) {
 // svg container
 var svgHeight = 400;
 var svgWidth = 700;
-
 // margins
 var margin = {
   top: 50,
   right: 50,
-  bottom: 50,
+  bottom: 10,
   left: 50
 };
-
 // chart area minus margins
 var chartHeight = svgHeight - margin.top - margin.bottom;
 var chartWidth = svgWidth - margin.left - margin.right;
-
 // create svg container
 var svg = d3.select("#svg-area").append("svg")
   .attr("height", svgHeight)
-  .attr("width", svgWidth);
-
+  .attr("width", svgWidth)
+  .attr("class", "graph-svg-component");;
 // shift everything over by the margins
 var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
-
 // scale y to chart height
 var yScale = d3.scaleLinear()
   .domain([0, d3.max(dataNumber)])
   .range([chartHeight, 0]);
-  console.log(d3.max(dataNumber));
-
+  // console.log(d3.max(dataNumber));
 // scale x to chart width
 var xScale = d3.scaleBand()
   .domain(dataTitle)
   .range([0, chartWidth])
   .padding(0.05);
-
 // create axes
 var yAxis = d3.axisLeft(yScale);
 var xAxis = d3.axisBottom(xScale);
-
 // set x to the bottom of the chart
 chartGroup.append("g")
   .attr("transform", `translate(0, ${chartHeight})`,)
   .call(xAxis)
+  .attr("class", "axisRed")
   .selectAll("text")	
         .style("text-anchor", "end")
         .attr("dx", "-.8em")
         .attr("dy", ".15em")
         .attr("transform", "rotate(-65)");
-
 chartGroup.append("g")
+  .attr("class", "axisRed")
   .call(yAxis);
-
 // Append Data to chartGroup
 chartGroup.selectAll(".bar")
-  .data(dataNumber)
+  .data(data)
   .enter()
   .append("rect")
   .classed("bar", true)
-  .attr("x", (d, i) => xScale(dataTitle[i]))
-  .attr("y", d => yScale(d))
+  .attr("x", d => xScale(d.title))
+  .attr("y", d => yScale(d.number))
   .attr("width", xScale.bandwidth())
-  .attr("height", d => chartHeight - yScale(d))
-  .attr("fill", "green")
+  .attr("height", d => chartHeight - yScale(d.number))
+  .attr("fill", function(d) {
+    if (d.number > 20000) {
+      return "#fe8476";
+    } else if (d.number> 18000) {
+      return "#fec176";
+    }
+    else if (d.number > 16000) {
+      return "#c5fe76";
+    }
+    else if (d.number > 14000) {
+      return "#81fe76";
+    }
+    else if (d.number > 12000) {
+      return "#76fecc";
+    }
+    else if (d.number> 10000) {
+      return "#76fcfe";
+    }
+    else if (d.number > 8000) {
+      return "#76cefe";
+    }
+    else if (d.number > 6000) {
+      return "#767dfe";
+    }
+    else if (d.number > 4000) {
+      return "#a176fe";
+    }
+    else if (d.number > 2000) {
+      return "#ee76fe";
+    }
+    return "#fe7676";
+  })
   .on("mousemove", function(d){
     tooltip
     .style("left", d3.event.pageX - 50 + "px")
     .style("top", d3.event.pageY - 70 + "px")
     .style("display", "inline-block")
-    .html((`# of Monuments: ${d}`));
+    .html("Title: " + d.title + "<br/>" + "Monuments Erected: " + d.number)
+   ;
+  //  console.log(d);
   })
-  .on("mouseout", function(d){ tooltip.style("display", "none");});;
-
-console.log("Logic3 file");
+  .on("mouseout", function(d){ tooltip.style("display", "none");});
+// console.log("Logic3 file");
  // Step 1: Initialize Tooltip
-
 var tooltip = d3.select("body")
                   .append("div")
                   .attr("class", "toolTip");
@@ -96,5 +123,5 @@ var tooltip = d3.select("body")
 
 });
 
-
+}, 3500);
  
